7ef28241b884cc041879b22fb137857532c8b77a
java_me <antaraj@bu.edu>
2020-09-29 15:28:53 -0500

    working on BST

--- data-structures-binary-search-tree-starter/lib/bst.js
+++ data-structures-binary-search-tree-starter/lib/bst.js
@@ -5,12 +5,30 @@ class TreeNode {
         this.right = null;
     }
 }
-//comment
-//
-//y
 
 class BST {
+    constructor() {
+        this.root = null;
+    }
 
+    insert(val) {
+        if (!this.root) { //no root, make a new tree node
+            this.root = new TreeNode(val)
+        }
+        let newNode = new TreeNode(val)
+        let current = this.root;
+        while (current) {
+            if (val < current.val) {
+                if (!current.left) {
+                    current.left = newNode;
+                    return this;
+                } else {
+                    current.left = new TreeNode(val)
+                    current.left.val = val;
+                }
+            }
+        }
+    }
 }
 
 module.exports = {
