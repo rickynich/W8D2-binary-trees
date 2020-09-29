21c80e22a5f08b05882909285f4bf6effa897421
Nick Richard <nrich414@gmail.com>
2020-09-29 16:47:04 -0400

    work on insert method

--- data-structures-binary-search-tree-starter/lib/bst.js
+++ data-structures-binary-search-tree-starter/lib/bst.js
@@ -11,23 +11,42 @@ class BST {
         this.root = null;
     }
 
-    insert(val) {
-        if (!this.root) { //no root, make a new tree node
-            this.root = new TreeNode(val)
-        }
+    insert(val, current = this.root) {
         let newNode = new TreeNode(val)
-        let current = this.root;
-        while (current) {
-            if (val < current.val) {
-                if (!current.left) {
-                    current.left = newNode;
-                    return this;
-                } else {
-                    current.left = new TreeNode(val)
-                    current.left.val = val;
-                }
+        if (!this.root) { //no root, make a new tree node
+            this.root = newNode
+        }
+        if (val < current.val) {
+            if (!current.left) {
+                current.left = newNode
+            } else {
+                this.insert(val, current.left)
+            } 
+        } else {
+            if (!current.right) {
+                current.right = newNode
+            } else {
+                this.insert(val, current.right)
             }
         }
+        // 
+        // while (current) {
+        //     if (val < current.val) {
+        //         if (!current.left) {
+        //             current.left = newNode;
+        //             return this;
+        //         }
+        //         // current = 
+        //         else {
+        //             if(current.right )
+        //             current.left = new TreeNode(val)
+        //             current.left.val = val;
+        //         }
+        //     }
+        //     current = false 
+        // }
+
+        return
     }
 }
 
